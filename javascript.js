edcc4d1ebad448d2af95820a70be65bfb25cfaa6
Robert Borland <50889172+LombaxRob@users.noreply.github.com>
2025-07-09 09:46:49 +0100

    First Commit

---
+++ javascript.js
@@ -0,0 +1,45 @@
+// Example 1
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+// Example 2
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+// Example 3
+const pi = 3.14;
+// pi = 10; causes error because you can't re-assign to a constant
+
+console.log(pi);
+
+// Example 4
+console.log((3+2)-76*(1+1));
+
+// Assignment 1
+console.log(23+97);
+
+// Assignment 2
+console.log(1+2+3+4+5+6);
+
+// Assignment 3
+console.log((4+6+9)/77);
+
+// Assignment 4
+let a = 10;
+console.log(a);
+a = 11;
+console.log(a);
+let b = 7 * a;
+console.log(b);
+
+// Assignment 5
+let max = 57;
+let actual = max -13;
+const percentage = actual/max;
+console.log(percentage);
