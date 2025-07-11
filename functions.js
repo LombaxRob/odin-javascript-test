ac7568f0220b6e2409392efd5df81fc9fcf0cc76
Robert Borland <50889172+LombaxRob@users.noreply.github.com>
2025-07-11 14:11:40 +0100

    feat: function basic tasks & index update
    Completed the short tasks given on the functions
    basics section. Created a new javascript file for
    the answers so I added the code to link that file
    into index.html

---
+++ functions.js
@@ -0,0 +1,31 @@
+// Task 1
+function add7(number) {
+    number = number + 7;
+    return number;
+}
+
+console.log(add7(5));
+
+// Task 2
+function multiply(num1, num2) {
+    answer = num1 * num2;
+    return answer;
+}
+
+console.log(multiply(2,3));
+
+// Task 3
+function capitalize(wrongCaseWord) {
+    wrongCaseWord = wrongCaseWord.toUpperCase();
+    return wrongCaseWord;
+}
+
+console.log(capitalize("ALJjejdjfjJSJjsjr"));
+
+// Task 4
+function lastLetter(word){
+    letter = word.charAt(word.length-1);
+    return letter;
+}
+
+console.log(lastLetter("hello"))
